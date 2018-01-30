0d563c0edb479e73148882e31bcc1856208f20d2
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-30 16:47:40 -0200

    Added HTTP re-routing

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/latitude-RESTsec-lab/api-gingonic/controllers"
 	"github.com/latitude-RESTsec-lab/api-gingonic/db"
 
@@ -8,6 +11,9 @@ import (
 )
 
 func main() {
+
+	// BEGIN HTTPS
+
 	httpsRouter := gin.Default()
 
 	db.Init()
@@ -18,5 +24,21 @@ func main() {
 	httpsRouter.GET("/api/servidor/:matricula", servidor.GetServidorMat) //Route with URL parameter
 	httpsRouter.POST("/api/servidor/", servidor.PostServidor)
 
+	// BEGIN HTTP
+	httpRouter := gin.Default()
+
+	httpRouter.GET("/", func(c *gin.Context) {
+		log.Print(c.Request.Host)
+		log.Print(c.Request.URL.Path)
+		c.JSON(200, nil)
+	})
+	httpRouter.GET("/api/servidores/", func(c *gin.Context) {
+		c.Redirect(302, fmt.Sprint("https://", c.Request.Host, ".", c.Request.URL.Path))
+	})
+	httpRouter.GET("/api/servidor/:matricula", func(c *gin.Context) {
+		c.Redirect(302, fmt.Sprint("https://", c.Request.Host, ".", c.Request.URL.Path))
+	})
+
+	go httpRouter.Run(":80")
 	httpsRouter.RunTLS(":443", "./devssl/server.pem", "./devssl/server.key") // listen and serve on 0.0.0.0:8080
 }
