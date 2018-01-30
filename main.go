a2710b3a13fdca4d8d3476ee6bb76b6c6b388ef3
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-30 15:41:01 -0200

    Changed HTTP to HTTPS

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-	router := gin.Default()
+	httpsRouter := gin.Default()
 
 	db.Init()
 	defer db.GetDB().Db.Close()
 	servidor := new(controllers.ServidorController) //Controller instance
 
-	router.GET("/api/servidores", servidor.GetServidores)           //Simple route
-	router.GET("/api/servidor/:matricula", servidor.GetServidorMat) //Route with URL parameter
-	router.POST("/api/servidor/", servidor.PostServidor)
+	httpsRouter.GET("/api/servidores", servidor.GetServidores)           //Simple route
+	httpsRouter.GET("/api/servidor/:matricula", servidor.GetServidorMat) //Route with URL parameter
+	httpsRouter.POST("/api/servidor/", servidor.PostServidor)
 
-	router.Run() // listen and serve on 0.0.0.0:8080
+	httpsRouter.RunTLS(":443", "./devssl/server.pem", "./devssl/server.key") // listen and serve on 0.0.0.0:8080
 }
